883062bb3b9d67246c165c333d60f4115c702712
farhan sharif <farhansharif1410@gmail.com>
2022-11-03 23:42:13 +0530

    services id added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -25,6 +25,13 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services)
 
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const services = await servicesCollection.findOne(query);
+            res.send(services)
         })
     }
     finally {
